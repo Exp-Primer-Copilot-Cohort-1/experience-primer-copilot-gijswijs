0be09e1483641a48c9a6e868b859fe6a32a187a5
Gijs van Dam <gvandam@gmail.com>
2023-11-07 22:14:55 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var div = document.getElementById("skills");
+    div.innerHTML += "Member";
+    div.style.marginBottom = "1%";
+    div.style.color = "#000000";
+    div.style.fontSize = "1.5em";
+    div.style.fontWeight = "bold";
+    div.style.fontFamily = "Arial";
+    div.style.textAlign = "center";
+}
